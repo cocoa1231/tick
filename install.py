715b6477e1256c939e7ff17728fe4490caf9aff5
cocoa1231 <cocoa1231@disroot.org>
2018-09-04 03:47:53 +0530

    add install.py

---
+++ install.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import os, platform, sys
+
+def get_platform():
+    if not '--platform' in sys.argv:
+        try:
+            pform = platform.dist()[0].lower()
+            return pform
+        except SyntaxError:
+            print("Fatal! Could not get platform, please pass it in by the --platform flag")
+            raise Exception("Could not get platform")
+
+    pform = sys.argv[ sys.argv.index('--platform') + 1 ] # Get the next item to the platform flag
+
+    return pform
+
+
+def install_ubuntu():
+    apt_packages = 'colordiff python3-pip'
+    if os.system('sudo apt install {}'.format(apt_packages)):
+        raise Exception("Failed to install dependencies")
+    
+    if os.system('sudo pip install -r requirements.txt'):
+        raise Exception("Failed to install dependencies")
+
+    if os.system('chmod +x init.py'):
+        raise Exception("Failed to mark init.py as executable. Check permissions")
+
+    return True 
+
+
+def install_arch():
+    pacman_packages = 'colordiff python-pip'
+    if os.system('sudo pacman -S {}'.format(pacman_packages)):
+        raise Exception("Failed to install dependencies")
+    
+    if os.system('sudo pip install -r requirements.txt'):
+        raise Exception("Failed to install dependencies")
+
+    if os.system('chmod +x init.py'):
+        raise Exception("Failed to mark init.py as executable. Check permissions")
+
+    return True 
+
+
+if __name__ == '__main__':
+    pform = get_platform()
+
+    # Python's version to a switch-case
+    install_type = { 'ubuntu' : install_ubuntu,
+                     'arch'   : install_arch     }
+
+    # Run the installation
+    install_type[pform]()
